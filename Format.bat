3e59378be4512be8674ba38dad2aedcbf0269a9e
YSH ALSAGER <ysh-alsager@hotmail.com>
2017-02-27 09:23:19 +0200

    Fix Hotspot Problem

--- Format.bat
+++ Format.bat
@@ -9,6 +9,9 @@ echo ./umount -l /dev/block/mmcblk0p22 >> %p%
 echo ./mke2fs -b 4096 -T ext4 /dev/block/mmcblk0p18 >> %p%
 echo ./mke2fs -b 4096 -T ext4 /dev/block/mmcblk0p19 >> %p%
 echo ./mke2fs -b 4096 -T ext4 /dev/block/mmcblk0p22 >> %p%
+echo ./mke2fs -b 4096 -T ext4 /dev/block/mmcblk0p23 >> %p%
+echo ./mke2fs -b 4096 -T ext4 /dev/block/mmcblk0p25 >> %p%
+echo ./mke2fs -b 4096 -T ext4 /dev/block/mmcblk0p26 >> %p%
 echo busybox mkfs.vfat /dev/block/mmcblk0p21 >> %p%
 echo ./mount "/cache" >> %p%
 echo exit >> %p%
